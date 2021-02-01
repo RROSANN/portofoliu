657d7fd5a5771e57f2488b965e0d2e75b04ef4d1
RROSANN <74143411+RROSANN@users.noreply.github.com>
2021-02-01 20:03:09 +0200

    new versas

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,17 +10,20 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  name: 'Ionut',
+  subtitle: "I'm your future Developer",
+  cta: 'Tell me more',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  paragraphOne:
+    "Hi! I’m Ionut Rosan. I’m a smart coder, savvy business person, and a funny guy. I love using a variety of tools to solve problems. I got a bachelor's in Robotics. I’m curently working as a mechanical design enginner for a company called ARRK. I started learning progamable languages when I saw how fun was to build an app that would open a door. I know it's funny how did u get from opening a door to developing websites, hang around and you will know more.",
+  paragraphTwo:
+    'When it comes to technology I’m most interested in using the power of programmatic language to give users new verbs: using code to empower people to do new amazing things. Code is pretty much the magic of our time and I would love being a modern day wizard!',
+  paragraphThree:
+    'When I am not working or coding I like to inform myself about what I could improve on my car and how can I DYI it, how car industry is moving, what are the new trends of the stock market.',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -28,30 +31,14 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'project1.PNG',
+    title: 'Face Recognition',
+    info:
+      'The aplication was buildt using React and is connected to the backend using node.js/Express.js and for database I used postgress.',
+    info2:
+      "In order to make it work you will create an account, doesn't need to be a true email and you won't receive emails or anything. After that just insert the link of an image that has a portrait and detect it.",
+    url: 'https://rrosann.github.io/facerecognition/',
+    repo: 'https://github.com/RROSANN/facerecognition/tree/master', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +46,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'ionut.rosan.96@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,22 +55,22 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/IonutRosan1',
     },
     {
       id: nanoid(),
-      name: 'codepen',
-      url: '',
+      name: 'facebook',
+      url: 'https://www.facebook.com/rosan.ionut.96',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/ionu%C8%9B-adrian-ro%C8%99an-030846146/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/RROSANN',
     },
   ],
 };
